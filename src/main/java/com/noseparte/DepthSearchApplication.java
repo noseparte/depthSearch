3e6276f87b87f0f67785ec9727d20edfa0aa7f00
Noseparte <noseparte@aliyun.com>
2018-12-20 14:35:56 +0800

    Initial commit

---
+++ src/main/java/com/noseparte/DepthSearchApplication.java
@@ -0,0 +1,14 @@
+package com.noseparte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepthSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepthSearchApplication.class, args);
+	}
+
+}
+
